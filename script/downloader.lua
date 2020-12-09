7a60bd61633d83fb5f7169b0af5270d09e21109b
Simon Hauser <Simon-Hauser@outlook.de>
2020-12-09 23:28:53 +0100

    Move to new plenary curl api

--- script/downloader.lua
+++ script/downloader.lua
@@ -1,19 +1,5 @@
 local Job = require'plenary.job'
-
-local download_file = function(source, callback)
-  local output = {}
-  Job:new {
-    command = 'curl',
-    args = { '-s', source },
-    on_stdout = function(_, line)
-      line = callback(line)
-      if line then
-        table.insert(output, line)
-      end
-    end
-  }:sync()
-  return output
-end
+local curl = require'plenary.curl'
 
 local format_json = function(file)
   local output = {}
@@ -69,77 +55,80 @@ local utf8 = function(cp)
 end
 
 local get_emoji_source = function()
-  local source = download_file('https://www.unicode.org/Public/emoji/13.1/emoji-test.txt', function(line)
+  local source = vim.split(curl.get('https://www.unicode.org/Public/emoji/13.1/emoji-test.txt').body, '\n')
+  local mod = {}
+  for _, line in ipairs(source) do
     if not line:find('; fully.qualified') then -- only emojis
-      return nil
+      goto continue
     end
     if line:find([[‍]]) then -- gets rid of concatenates emojis.
-      return nil
+      goto continue
     end
     -- skin/hair colors which are used to create some emojis in different colors. Useless for us
     local skin = { '🏻', '🏼', '🏽', '🏾', '🏿', '🦰', '🦱', '🦳', '🦲' }
     for _, v in ipairs(skin) do
       if line:find(v) then
-        return nil
+        goto continue
       end
     end
     line = line:gsub('.* # ', '')
     line = line:gsub(' E[0-9]*.[0-9]', '')
     line = line:gsub('%s', '   ', 1)
     local symbol, description = line:match('(.+)%s%s%s(.*)')
-    return { symbol, description }
-  end)
-  local json = vim.fn.json_encode(source)
+    table.insert(mod, { symbol, description })
+    ::continue::
+  end
+  local json = vim.fn.json_encode(mod)
   write_to_file(json, 'emoji.json')
 end
 
 local get_math_source = function()
-  local download_source = 'https://raw.githubusercontent.com/wspr/unicode-math/ef5688f303d7010138632ab45ef2440d3ca20ee5/unicode-math-table.tex'
-  local source = download_file(download_source, function(line)
-    if line:sub(1, 8) ~= [[\Unicode]] then
-      return nil
+  local source = vim.split(curl.get('https://raw.githubusercontent.com/wspr/unicode-math/ef5688f303d7010138632ab45ef2440d3ca20ee5/unicode-math-table.tex').body, '\n')
+  local mod = {}
+  for _, line in ipairs(source) do
+    if line:sub(1, 8) == [[\Unicode]] then
+      local symbol, description = line:match('.*{(.*)}{.*}{.*}{(.*)}')
+      symbol = symbol:gsub('"', '0x')
+      symbol = tonumber(symbol)
+      symbol = utf8(symbol)
+      table.insert(mod, { symbol, description })
     end
-    local symbol, description = line:match('.*{(.*)}{.*}{.*}{(.*)}')
-    symbol = symbol:gsub('"', '0x')
-    symbol = tonumber(symbol)
-    symbol = utf8(symbol)
-    return { symbol, description }
-  end)
-  local json = vim.fn.json_encode(source)
+  end
+  local json = vim.fn.json_encode(mod)
   write_to_file(json, 'math.json')
 end
 
 local get_latex_source = function()
-  local download_source = 'https://raw.githubusercontent.com/wspr/unicode-math/ef5688f303d7010138632ab45ef2440d3ca20ee5/unicode-math-table.tex'
-  local source = download_file(download_source, function(line)
-    if line:sub(1, 8) ~= [[\Unicode]] then
-      return nil
+  local source = vim.split(curl.get('https://raw.githubusercontent.com/wspr/unicode-math/ef5688f303d7010138632ab45ef2440d3ca20ee5/unicode-math-table.tex').body, '\n')
+  local mod = {}
+  for _, line in ipairs(source) do
+    if line:sub(1, 8) == [[\Unicode]] then
+      local symbol, description = line:match('.*{.*}{(.*)}{.*}{(.*)}')
+      symbol = symbol:gsub('%s*$', '')
+      table.insert(mod, { symbol, description })
     end
-    local symbol, description = line:match('.*{.*}{(.*)}{.*}{(.*)}')
-    symbol = symbol:gsub('%s*$', '')
-    return { symbol, description }
-  end)
-  local json = vim.fn.json_encode(source)
+  end
+  local json = vim.fn.json_encode(mod)
   write_to_file(json, 'latex.json')
 end
 
 local get_kaomoji_source = function()
-  local download_source = 'https://raw.githubusercontent.com/kuanyui/kaomoji.el/90a1490743b2a30762f5454c9d9309018eff83dd/kaomoji-data.el'
-  local source = download_file(download_source, function(line)
-    if not line:find('%s%.%s') then
-      return nil
+  local source = vim.split(curl.get('https://raw.githubusercontent.com/kuanyui/kaomoji.el/90a1490743b2a30762f5454c9d9309018eff83dd/kaomoji-data.el').body, '\n')
+  local mod = {}
+  for _, line in ipairs(source) do
+    if line:find('%s%.%s') then
+      local tmp_table = vim.split(line, ' %. ')
+      tmp_table[1] = tmp_table[1]:match('^%s*\'*%(*(.*)%)%s*$')
+      tmp_table[1] = tmp_table[1]:gsub('"', '')
+      tmp_table[1] = tmp_table[1]:gsub('%s*$', '') -- There are also trailing whitespaces inside ()
+
+      tmp_table[2] = tmp_table[2]:match('^"(.+)"%s*%)$')
+
+      line = tmp_table[2] .. '   ' .. tmp_table[1]
+      table.insert(mod, { tmp_table[2], tmp_table[1] })
     end
-    local tmp_table = vim.split(line, ' %. ')
-    tmp_table[1] = tmp_table[1]:match('^%s*\'*%(*(.*)%)%s*$')
-    tmp_table[1] = tmp_table[1]:gsub('"', '')
-    tmp_table[1] = tmp_table[1]:gsub('%s*$', '') -- There are also trailing whitespaces inside ()
-
-    tmp_table[2] = tmp_table[2]:match('^"(.+)"%s*%)$')
-
-    line = tmp_table[2] .. '   ' .. tmp_table[1]
-    return { tmp_table[2], tmp_table[1] }
-  end)
-  local json = vim.fn.json_encode(source)
+  end
+  local json = vim.fn.json_encode(mod)
   write_to_file(json, 'kaomoji.json')
 end
 
